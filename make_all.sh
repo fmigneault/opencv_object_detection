240eb19934121dfa3607f2e47050b71beb5b2783
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2018-01-16 16:55:45 -0500

    util script make all sub-projects

---
+++ make_all.sh
@@ -0,0 +1,16 @@
+cur_dir=$(pwd)
+bld_dir=bld
+cmake_bld_type=Release
+cmake_install=$cur_dir/install
+programs="jetson_tegra_gstreamer_camera ssd_mobilenet_opencv ssd_opencv yolo_opencv"
+
+for prog in $programs
+do
+    cur_bld=$cur_dir/$prog/$bld_dir
+    mkdir -p $cur_bld
+    cd $cur_bld
+    cmake .. -DCMAKE_BUILD_TYPE=$cmake_bld_type -DCMAKE_INSTALL_PREFIX=$cmake_install
+    make -j 4
+    make install
+done
+
