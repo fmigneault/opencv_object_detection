4448ff9548624e889046694ada557dabd3521494
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2018-02-14 16:02:00 -0500

    try making inception with video [WIP-random]

--- inception_opencv/src/tf_inception.cpp
+++ inception_opencv/src/tf_inception.cpp
@@ -25,12 +25,18 @@ const String keys =
                        "The model and class names list can be downloaded here: "
                        "https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip }"
         "{model m   |tensorflow_inception_graph.pb| path to TensorFlow .pb model file }"
-        "{image i   || path to image file }"
+        "{input i   || path to image or video file }"
+        "{cam_dev   | 0 | camera device index }"
         "{i_blob    | input | input blob name) }"
         "{o_blob    | softmax2 | output blob name) }"
         "{c_names c | imagenet_comp_graph_label_strings.txt | path to file with classnames for class id }"
+        "{t_conf t  | 0.2 | confidence threshold of detections }"
         "{result r  || path to save output blob (optional, binary format, NCHW order) }"
         ;
+
+const size_t inSize = 224;
+const float inScaleFactor = 0.007843f;
+const float meanVal = 117.0;        
 
 void getMaxClass(const Mat &probBlob, int *classId, double *classProb);
 std::vector<String> readClassNames(const char *filename);
@@ -46,9 +52,10 @@ int main(int argc, char **argv)
     }
 
     String modelFile = parser.get<String>("model");
-    String imageFile = parser.get<String>("image");
+    String inputFile = parser.get<String>("input");
     String inBlobName = parser.get<String>("i_blob");
     String outBlobName = parser.get<String>("o_blob");
+    float confidenceThreshold = parser.get<float>("t_conf");
 
     if (!parser.check())
     {
@@ -69,53 +76,189 @@ int main(int argc, char **argv)
         std::cerr << modelFile << std::endl;
         exit(-1);
     }
-
-    //! [Prepare blob]
-    Mat img = imread(imageFile);
-    if (img.empty())
+    
+    std::vector<String> classNames;
+    if (!classNamesFile.empty())
+        classNames = readClassNames(classNamesFile.c_str());
+    else
     {
-        std::cerr << "Can't read image from the file: " << imageFile << std::endl;
+        std::cerr << "Missing class names file" << std::endl;
         exit(-1);
     }
-
-    Mat inputBlob = blobFromImage(img, 1.0f, Size(224, 224), Scalar(), true, false);   //Convert Mat to batch of images
-    //! [Prepare blob]
-    inputBlob -= 117.0;
-    //! [Set input blob]
-    net.setInput(inputBlob, inBlobName);        //set the network input
-    //! [Set input blob]
-
-    cv::TickMeter tm;
-    tm.start();
-
-    //! [Make forward pass]
-    Mat result = net.forward(outBlobName);                          //compute output
-    //! [Make forward pass]
-
-    tm.stop();
-
-    if (!resultFile.empty()) {
-        CV_Assert(result.isContinuous());
-
-        ofstream fout(resultFile.c_str(), ios::out | ios::binary);
-        fout.write((char*)result.data, result.total() * sizeof(float));
-        fout.close();
+    if (classNames.size() < 1)
+    {
+        std::cerr << "Could not read class names from file: '" << classNamesFile << "'" << std::endl;
+        exit(-1);
     }
+    
+    VideoCapture cap;
+    bool is_camera = false;
+    if (parser.get<String>("input").empty())
+    {
+        int cameraDevice = parser.get<int>("camera_device");
+        cap = VideoCapture(cameraDevice);
+        if(!cap.isOpened())
+        {
+            cout << "Couldn't find camera: " << cameraDevice << endl;
+            return -1;
+        }
+        is_camera = true;
+    }
+    else
+    {
+        String input = parser.get<String>("input");
+        cap.open(input);
+        if(!cap.isOpened())
+        {
+            cout << "Couldn't open image or video: " << input << endl;
+            return -1;
+        }
+    }   
+    
+    String display_title = "Inception: Detections";
+    namedWindow(display_title, WINDOW_NORMAL | WINDOW_FREERATIO | CV_GUI_EXPANDED);
+    resizeWindow(display_title, 800, 600); 
 
-    std::cout << "Output blob shape " << result.size[0] << " x " << result.size[1] << " x " << result.size[2] << " x " << result.size[3] << std::endl;
-    std::cout << "Inference time, ms: " << tm.getTimeMilli()  << std::endl;
+    size_t frame_count = 0;
+    cv::TickMeter tm;
+    Mat result, frame, image;
+    ostringstream ss_label;
+    for (;;) 
+    {        
+        //! [Prepare blob]
+        cap >> frame;
+        
+        if (frame.empty())
+        {
+            waitKey(1);
+            break;
+        }
+        
+        if (frame.empty())
+        {
+            if (is_camera)
+                std::cerr << "Can't read frame from camera device" << std::endl;
+            else
+                std::cerr << "Can't read frame from file: " << inputFile << std::endl;
+            exit(-1);
+        }
+        frame_count++;
+        
+        //! [Prepare blob]
+        if (frame.channels() == 4)
+            cvtColor(frame, frame, COLOR_BGRA2BGR);
+        
+        //Convert frame to batch of images
+        Mat inputBlob = blobFromImage(frame, inScaleFactor, Size(inSize, inSize), 
+                                      Scalar(meanVal, meanVal, meanVal), true, false); 
+        //! [Prepare blob]
+        
+        //! [Set input blob]
+        net.setInput(inputBlob, inBlobName);    //set the network input
+        //! [Set input blob]
+        
+        tm.start();
 
-    if (!classNamesFile.empty()) {
-        std::vector<String> classNames = readClassNames(classNamesFile.c_str());
+        //! [Make forward pass]
+        result = net.forward(outBlobName);  //compute output
+        //! [Make forward pass]
+
+        tm.stop();
+        ostringstream ss;
+        vector<double> layersTimings;
+        double freq = getTickFrequency() / 1000;
+        double time = net.getPerfProfile(layersTimings) / freq;
+        ss << "FPS: " << 1000/time << " ; time: " << time << " ms";        
+        putText(frame, ss.str(), Point(20,20), 0, 0.5, Scalar(0,0,255));
+        if (frame_count == 1) 
+        {
+            ss_label << ss.str() << " ; label: ";        
+            image = frame;
+        }
+        
+        Mat detections(result.size[2], result.size[3], CV_32F, result.ptr<float>());                
+        std::cout << "===" << std::endl 
+            << result.size() << std::endl
+            << result.size << std::endl 
+            << detections.size() << std::endl 
+            << detections.rows << std::endl 
+            << detections.at<float>(0, 3) * frame.cols << ", " 
+            << detections.at<float>(0, 4) * frame.rows << ", " 
+            << detections.at<float>(0, 5) * frame.cols << ", " 
+            << detections.at<float>(0, 6) * frame.rows << ", " << std::endl;
+        
+        for(int i = 0; i < detections.rows; i++)
+        {
+            float confidence = detections.at<float>(i, 2);
+            if(confidence > confidenceThreshold)
+            {
+                int xLeftBottom = static_cast<int>(detections.at<float>(i, 3) * frame.cols);
+                int yLeftBottom = static_cast<int>(detections.at<float>(i, 4) * frame.rows);
+                int xRightTop = static_cast<int>(detections.at<float>(i, 5) * frame.cols);
+                int yRightTop = static_cast<int>(detections.at<float>(i, 6) * frame.rows);
+                
+                Rect object((int)xLeftBottom, (int)yLeftBottom,
+                            (int)(xRightTop - xLeftBottom),
+                            (int)(yRightTop - yLeftBottom));
+                rectangle(frame, object, Scalar(0, 255, 0));
+                
+                std::cout << " OBJ: " << object << " | ";
+                
+                   
+                ss.str("");
+                ss << confidence;
+                String conf(ss.str());                
+                int classId;
+                double classProb;
+                getMaxClass(result, &classId, &classProb);//find the best class
+                String label = classNames.at(classId);
+                int baseLine = 0;
+                Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+                rectangle(frame, Rect(Point(xLeftBottom, yLeftBottom - labelSize.height),
+                                      Size(labelSize.width, labelSize.height + baseLine)),
+                          Scalar(255, 255, 255), FILLED);
+                putText(frame, label, Point(xLeftBottom, yLeftBottom),
+                        FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0,0,0));
+            }                        
+        }
+        
+        imshow(display_title, frame);
+        if (waitKey(1) >= 0) break;
+    }
+    
+    if (frame_count == 1)
+    {
+        if (!resultFile.empty()) 
+        {
+            CV_Assert(result.isContinuous());
+
+            ofstream fout(resultFile.c_str(), ios::out | ios::binary);
+            fout.write((char*)result.data, result.total() * sizeof(float));
+            fout.close();
+        }
+
+        std::cout << "Output blob shape " 
+                  << result.size[0] << " x " 
+                  << result.size[1] << " x " 
+                  << result.size[2] << " x " 
+                  << result.size[3] << std::endl;
+        std::cout << "Inference time, ms: " << tm.getTimeMilli() << std::endl;
 
         int classId;
         double classProb;
         getMaxClass(result, &classId, &classProb);//find the best class
-
+        String label = classNames.at(classId);
+        ss_label << label;
+        putText(image, ss_label.str(), Point(20,20), 0, 0.5, Scalar(0,0,255));
+        imshow(display_title, image);
+        
         //! [Print results]
-        std::cout << "Best class: #" << classId << " '" << classNames.at(classId) << "'" << std::endl;
+        std::cout << "Best class: #" << classId << " '" << label << "'" << std::endl;
         std::cout << "Probability: " << classProb * 100 << "%" << std::endl;
+        
+        waitKey();
     }
+        
     return 0;
 } //main
 
--- inception_opencv_sample.sh
+++ inception_opencv_sample.sh
@@ -3,11 +3,15 @@
 # ========== 
 install_dir=/home/nvidia/opencv_object_detection/install/bin
 opencv_path=/home/nvidia/opencv
-input_path=$opencv_path/samples/data/fruits.jpg
-#input_path=$opencv_path/samples/data/vtest.avi
-#input_path=$opencv_path/samples/data/Megamind.avi
 models_dir=/home/nvidia/models
 label_path=$models_dir/labels/imagenet_comp_graph_label_strings.txt 
+
+# ==========
+# input 
+# ========== 
+input_path=$opencv_path/samples/data/fruits.jpg
+input_path=$opencv_path/samples/data/vtest.avi
+input_path=$opencv_path/samples/data/Megamind.avi
 
 # ========== 
 # general config options 
@@ -19,16 +23,13 @@ model_file=tensorflow_inception_graph.pb
 # ==========
 # commands with options 
 # ==========
-#cmd="$install_dir/yolo_object_detection -video=0 -cfg=$models_dir/$model.cfg -model=$models_dir/$model.weights -class_names=$label_path -scale_factor=$scale_factor -input_size=$model_input_size"
-cmd="$install_dir/tf_inception 
-    -image=$input_path -model=$models_dir/$model_file 
-    -c_names=$label_path -scale_factor=$scale_factor"
+cmd="$install_dir/tf_inception -input=$input_path -model=$models_dir/$model_file -t_conf=0.01 -c_names=$label_path -scale_factor=$scale_factor"
 cmd_txt=$(eval "echo $cmd")
 
 # ==========
 # summary and call command
 # ==========
-echo "model:   [$model]"
+echo "model:   [$model_file]"
 echo "scaling: [$scale_factor]"
 echo "input:   [$input_path]"
 echo "cmd:     [$cmd]"
