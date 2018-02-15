568e0642f2733df35a1d8025032348589b03934c
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2018-02-15 10:30:55 -0500

    fix jetson camera sample with independant include

---
+++ jetson_tegra_gstreamer_camera/src/test_jetson_tegra_gstreamer_camera.cpp
@@ -0,0 +1,33 @@
+#include "jetson_tegra_gstreamer_camera.hpp"
+#include <opencv2/opencv.hpp>
+#include <string>
+
+
+int main() {
+    // Options
+    int WIDTH = 800;  //1920;
+    int HEIGHT = 600; //1080;
+    int FPS = 30;
+
+    // Define the gstream pipeline
+    std::string pipeline = get_tegra_pipeline(WIDTH, HEIGHT, FPS);
+    std::cout << "Using pipeline: \n\t" << pipeline << "\n";
+
+    // Create OpenCV capture object, ensure it works.
+    cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
+    if (!cap.isOpened()) {
+        std::cout << "Connection failed";
+        return -1;
+    }
+
+    // View video
+    cv::Mat frame;
+    while (1) {
+        cap >> frame;  // Get a new frame from camera
+
+        // Display frame
+        imshow("Display window", frame);
+        cv::waitKey(1); //needed to show frame
+    }
+}
+
